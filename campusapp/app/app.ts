6d6f26977aaa529507d17ef3a0728c6bc3c74f78
saurabh vyas <saurabh vyas>
2016-08-14 12:40:46 +0530

    updated auth provider added firebase

--- campusapp/app/app.ts
+++ campusapp/app/app.ts
@@ -13,6 +13,27 @@ export class MyApp {
   rootPage: any = HomePage;
 
   constructor(platform: Platform) {
+
+    var config = {
+    apiKey: "AIzaSyDghKMAJMm5DB5OOB9ws2ZZ-eM2xKS8GCQ",
+    authDomain: "elearning-a3ed2.firebaseapp.com",
+    databaseURL: "https://elearning-a3ed2.firebaseio.com",
+    storageBucket: "elearning-a3ed2.appspot.com",
+  };
+
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // If there's a user take him to the home page.
+    this.rootPage = HomePage;
+  } else {
+    // If there's no user logged in send him to the LoginPage
+    this.rootPage = LoginPage;
+  }
+});
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- campusapp/app/providers/auth-data/auth-data.ts
+++ campusapp/app/providers/auth-data/auth-data.ts
@@ -1,17 +1,36 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-
-/*
-  Generated class for the AuthData provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
+import {Injectable} from '@angular/core';
+import * as firebase from 'firebase';
 @Injectable()
 export class AuthData {
 
-  constructor(private http: Http) {}
+   public fireAuth: any;
+  public userProfile: any;
 
+
+  constructor() {
+
+
+    this.fireAuth = firebase.auth();
+  this.userProfile = firebase.database().ref('/userProfile')
+
+  }
+
+   loginUser(email: string, password: string): any {
+    return this.fireAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  signupUser(email: string, password: string): any {
+    return this.fireAuth.createUserWithEmailAndPassword(email, password).then((newUser) => {
+      this.fireAuth.signInWithEmailAndPassword(email, password).then((authenticatedUser) => {
+        this.userProfile.child(authenticatedUser.uid).set({
+          email: email
+        });
+      });
+    });
+  }
+
+  logoutUser(): any {
+  return this.fireAuth.signOut();
 }
 
+}
