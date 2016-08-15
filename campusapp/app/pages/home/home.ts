e990d199b1c6fa7bcd67566d96e27d81c3dbf75c
saurabh vyas <saurabh vyas>
2016-08-15 22:00:28 +0530

    added background img

--- campusapp/app/pages/home/home.ts
+++ campusapp/app/pages/home/home.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import { AuthData } from '../../providers/auth-data/auth-data';
+import { LoginPage } from '../login/login';
+
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers:[AuthData]
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  constructor(public nav: NavController,public authData:AuthData) {
 
   }
+
+  logOut(){
+  this.authData.logoutUser().then(() => {
+    this.nav.setRoot(LoginPage);
+  });
+}
 }
--- campusapp/app/pages/login/login.ts
+++ campusapp/app/pages/login/login.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,LoadingController, AlertController } from 'ionic-angular';
+import { AuthData } from '../../providers/auth-data/auth-data';
+import { FormBuilder, Validators, ControlGroup } from '@angular/common';
+import { SignupPage } from '../signup/signup';
+import { HomePage } from '../home/home';
 
 /*
   Generated class for the LoginPage page.
@@ -9,11 +13,68 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   templateUrl: 'build/pages/login/login.html',
+  providers:[AuthData]
 })
 export class LoginPage {
 
-  constructor(private navCtrl: NavController) {
+  public loginForm: any;
+   submitAttempt: boolean = false;
+   email:any="";
+   password:any="";
+
+
+
+
+  constructor(private nav: NavController,public authData: AuthData,public formBuilder: FormBuilder,
+  public alertCtrl: AlertController,public loadingCtrl: LoadingController) {
+  
+  
+  
 
   }
+
+
+
+ 
+loginUser(){
+
+console.log(` email is ${this.email} and password is ${this.password}`);
+
+   // this.submitAttempt = true;
+
+  
+      this.authData.loginUser(this.email, this.password).then( authData => {
+        this.nav.setRoot(HomePage);
+      }, error => {
+        
+        console.log(`${error.message}`);
+        
+        /* let alert = this.alertCtrl.create({
+          message: error.message,
+          buttons: [
+            {
+              text: "Ok",
+              role: 'cancel'
+            }
+          ]
+        });
+        alert.present();
+        */
+      });
+      let loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      loading.present();
+    
+  }
+
+
+  goToSignup(){
+  this.nav.push(SignupPage);
+}
+
+goToResetPassword(){
+ // this.nav.push(ResetPasswordPage);
+}
 
 }
