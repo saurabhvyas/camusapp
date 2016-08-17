0187c8031cdaf33eb202260b4da277eb7acaf132
saurabh vyas <saurabh vyas>
2016-08-17 22:52:44 +0530

    getting error related to popover

--- campusapp/app/pages/home/home.ts
+++ campusapp/app/pages/home/home.ts
@@ -1,19 +1,37 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController,PopoverController} from 'ionic-angular';
 
 import { AuthData } from '../../providers/auth-data/auth-data';
 import { LoginPage } from '../login/login';
+import { PopoverPage } from '../popover/popover';
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
   providers:[AuthData]
 })
 export class HomePage {
-  constructor(public nav: NavController,public authData:AuthData) {
+
+  username:string = "Saurabh Vyas";
+  bio:string="Open Source Enthusiast and UI/UX Designer , Also has great interest in Education"
+  constructor(public nav: NavController,public authData:AuthData,private popover:PopoverController) {
 
   }
+  
+  presentpopover(myEvent) {
+    console.log('test');
+  
 
-  logOut(){
+ var mypopover=this.popover.create(PopoverPage);
+
+ 
+ mypopover.present({
+   ev:myEvent
+ });
+
+
+
+  }
+  logOut() {
   this.authData.logoutUser().then(() => {
     this.nav.setRoot(LoginPage);
   });
---
+++ campusapp/app/pages/popover/popover.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController,ViewController } from 'ionic-angular';
+
+/*
+  Generated class for the PopoverPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/popover/popover.html',
+})
+export class PopoverPage {
+
+  constructor(private navCtrl: NavController,private viewCtrl: ViewController) {
+
+  }
+
+}
